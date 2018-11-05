375100b8431a666a0b927d90a0ab007497ab774d
DymaxionMap <sthliao@gmail.com>
2018-11-05 15:43:25 -0800

    Complete getElementsByClassNameSpec and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  
+  /*
+  input => string of className
+  output => array of elements that contain 'className'
+  if (element contains className)
+    push to the element to array
+  base case - if (element doesn't have children)
+                return
+  recursive case - if (element has children) 
+                   for (each child) 
+                    getElementsByClassName(child)
+  
+  */
+  var elementsWithClass = [];
+  var checkElements = function(child) {
+    if (child.classList && child.classList.contains(className)) {
+      elementsWithClass.push(child);
+    }
+    
+    if (child.childNodes.length === 0) {
+      return;
+    }
+    
+    child.childNodes.forEach(function(element) {
+      checkElements(element);
+    });
+  };
+  checkElements(document.body);
+  return elementsWithClass;
 };
